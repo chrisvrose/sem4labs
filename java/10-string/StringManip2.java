ae13b297fe9c9564cfc18583c73728362e44aca2
Christopher V. Rose <christophervrose@gmail.com>
2020-03-25 22:18:36 +0530

    Work

--- java/10-string/StringManip2.java
+++ java/10-string/StringManip2.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 public class StringManip2 {
     public static void main(String[] args) throws ArrayIndexOutOfBoundsException {
-        System.out.println(new Scanner(System.in).nextLine().toLowerCase().replace("python", "_")
-                .replace("java", "python").replace("_", "java"));
+        System.out.println(new Scanner(System.in).nextLine().toLowerCase().replace("python", "_").replace("java", "python").replace("_", "java"));
     }
 }

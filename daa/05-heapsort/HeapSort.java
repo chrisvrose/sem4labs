a8f8d0664e2d7632a1b3ab73661ea0ef2ea425a6
Atreya Bain <christophervrose@gmail.com>
2020-06-02 21:13:30 +0530

    HeapSort

--- daa/05-heapsort/HeapSort.java
+++ daa/05-heapsort/HeapSort.java
@@ -1,104 +1,100 @@
+import java.util.Arrays;
 import java.util.Scanner;
-public class HeapSort{
-	private int[] a;
-	public	int count = 0;
-	//int count2 = 0;
-	public HeapSort(int[] heap){this.a  = heap;makeHeap();heapSort();}
 
+public class HeapSort {
+	protected int[] a;
+	public int count = 0;
 
-	public void heapUp(int endpoint){
-		int i=endpoint,t;
-		while(((i-1)/2)>=0){
-			//System.out.println(a[i]+" CHECK:"+i);
-			count++;
-			if(a[i]>a[(i-1)/2]){
-				t = a[i];
-				a[i] = a[(i-1)/2];
-				a[(i-1)/2] = t;
-			}else{
-				break;
-			}
-			//count++;
-			i = (i-1)/2;
-		}
+	public HeapSort(int[] heap) {
+		this.a = heap;
+		heapify();
+		heapSort();
 	}
 
+	// wrap it
+	void heapify() {
+		heapify(a.length - 1);
+	}
 
-	public void heapDown(int endpoint){
-		int i=0,maxChildIndex,t;
-		while(i<=endpoint){
-			if(2*i+1<=endpoint){
-				//has atleast 1 child
-				maxChildIndex = 2*i+1;
-				//count++;
-				if(2*i+2<=endpoint){
-					maxChildIndex = a[2*i+1]>a[2*i+2]?(2*i+1):(2*i+2);
+	// bottom up heap
+	void heapify(int n) {
+		int k, v = 0, j = 0;
+		boolean heap = false;
+		for (int i = n / 2; i > 0; i--) {
+			k = i;
+			v = a[k];
+			heap = false;
+
+			while (!heap && 2 * k <= n) {
+				j = 2 * k;
+				if (j < n) {
+					if (a[j] < a[j + 1])
+						j++;
 				}
 				count++;
-				if(a[i]<a[maxChildIndex]){
-					t = a[maxChildIndex];
-					a[maxChildIndex] = a[i];
-					a[i] = t;
+				if (v >= a[j]) {
+					heap = true;
+				} else {
+					a[k] = a[j];
+					k = j;
 				}
-				i = maxChildIndex;  
-			}else{
-				break;
+
 			}
-
+			// final swap
+			a[k] = v;
+			// System.out.println(i + "," + Arrays.toString(a));
 		}
 	}
 
-	public void makeHeap(){
-		for(int i=1;i<a.length;i++){
-			//till endpoint
-			heapUp(i);
-		}
-	}
-
-	public void heapSort(){
+	public void heapSort() {
 		int t;
-		for(int i=a.length-2;i>=0;i--){
-			t = a[0];
-			a[0] = a[i+1];
-			a[i+1] = t;
-			heapDown(i);
+		for (int i = a.length - 2; i >= 1; i--) {
+			t = a[1];
+			a[1] = a[i + 1];
+			a[i + 1] = t;
+			// System.out.println("RUN" + i);
+			heapify(i);
 		}
 
 	}
 
-	//public removeRoot(in
+	// public removeRoot(in
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		int n, i, j;
 		System.out.println("Enter number of elements");
-		int n = in.nextInt(),i,j;
-		int[] heapArray = new int[n];
-		for(i=0;i<n;i++){
+		n = in.nextInt();
+		int[] heapArray = new int[n + 1];
+		heapArray[0] = -1;
+		for (i = 1; i <= n; i++) {
 			heapArray[i] = in.nextInt();
 		}
+		in.close();
 		int newCount = new HeapSort(heapArray).count;
-		for(int x:heapArray){
-			System.out.print(x+" ");
+		for (i = 1; i <= n; i++) {
+			System.out.print(heapArray[i] + " ");
 		}
-		System.out.println("COUNT "+newCount);
+		System.out.println("COUNT " + newCount);
 
-		int[][] newArray = new int[3][];
-		int[] counts = new int[3];
-		System.out.println("n\tASC\tDESC\tRAND");
-		for(i=1;i<16;i++){
-			for(j=0;j<3;j++){
-				newArray[j] = new int[1<<i];
-			}
-			for(j=0;j<(1<<i);j++){
-				newArray[0][j] = j;
-				newArray[1][j] = 1<<i - j;
-				newArray[2][j] = (int)(Math.random()*(1<<i));
-			}
-			for(j=0;j<3;j++){
-				counts[j] = new HeapSort(newArray[j]).count;
-			}
-			n = newArray[0].length;
-			System.out.println(n+"\t"+counts[0]+"\t"+counts[1]+"\t"+counts[2]);
-		}
+		// int[][] newArray = new int[3][];
+		// int[] counts = new int[3];
+		// System.out.println("n\tASC\tDESC\tRAND");
+		// for(i=1;i<16;i++){
+		// for(j=0;j<3;j++){
+		// newArray[j] = new int[1<<i];
+		// }
+		// for(j=0;j<(1<<i);j++){
+		// newArray[0][j] = j;
+		// newArray[1][j] = 1<<i - j;
+		// newArray[2][j] = (int)(Math.random()*(1<<i));
+		// }
+		// for(j=0;j<3;j++){
+		// counts[j] = new HeapSort(newArray[j]).count;
+		// }
+		// n = newArray[0].length;
+		// System.out.println(n+"\t"+counts[0]+"\t"+counts[1]+"\t"+counts[2]);
+		// }
+
 	}
 }
